81b18b8de3c51db56967265effa790636aa809b6
nandofernandex <nando200604@outlook.com>
2024-06-18 20:57:39 -0300

    Atualizar .gitignore para ignorar arquivos e pastas especificados

--- scripts.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d8cdf91b4c3bf445a842d04db40bb28e2bc6e4b26408899812d589aeb56a8f69
-size 298
--- server.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:de96cb8a1e1a9bdd820b67926e8a60577684285c3f55246e24a3259d38c8c985
-size 6296
